4851924f660a1fe82720de3317f75dbb349ab732
curt <curt.pederson@gmail.com>
2021-02-18 19:26:19 -0600

    Decoupled sort order from the number

--- init_tracker/models/init.rb
+++ init_tracker/models/init.rb
@@ -6,10 +6,13 @@ module InitTracker
     class Init < ActiveRecord::Base
       belongs_to :server
 
+      ORDER_MULTIPLIER = 100
+
       serialize :characters, Array
 
       after_initialize :add_missing_attributes
 
+      # Rerolls the dice for all characters
       def reroll!
         characters.each do |char|
           set_character_init_order(char)
@@ -19,6 +22,7 @@ module InitTracker
         save
       end
 
+      # Resets the went and up status for all characters
       def reset!
         reset_went
         reset_up
@@ -90,6 +94,7 @@ module InitTracker
         save
       end
 
+      # Removes the character at the position provided
       def remove_character!(position_number)
         return if position_number < 1 || position_number > characters.size
         if characters[position_number - 1][:up]
@@ -99,6 +104,7 @@ module InitTracker
         save
       end
 
+      # Locates the character by their name and returns them if found
       def find_character_by_name(name)
         InitTrackerLogger.log.debug {"Find by name: #{name}"}
         characters.select{|char| char[:name].strip.downcase.eql?(name.strip.downcase)}.first
@@ -109,6 +115,7 @@ module InitTracker
         characters.find_index {|char| char[:key].eql?(character[:key])}
       end
 
+      # When the channel is removed, it will wipe out any init associated to it
       def self.channel_removed(event)
         InitTrackerLogger.log.debug {"Channel: channel removed: '#{event.server.name}' (ID: #{event.id})"}
         # Find any inits for this channel and blow them away.
@@ -117,31 +124,39 @@ module InitTracker
 
       private
 
+      # Returns true if all characters have gone
       def all_went?
-        InitTrackerLogger.log.debug {"characters: #{characters}"}
         yet_to_go = characters.detect {|character| character[:went] == false}
-        InitTrackerLogger.log.debug {"yet to go: #{yet_to_go}"}
         return !yet_to_go.present?
       end
 
+      # Resets the went status for all characters
       def reset_went
-        InitTrackerLogger.log.debug {"reset went"}
         characters.each do |character|
           character[:went] = false
         end
       end
 
+      # Sets the order for all characters based on the number assigned to them
+      def set_order
+        characters.each do |character|
+          character[:order] = character[:number] * ORDER_MULTIPLIER
+        end
+      end
+
+      # Resets the up status for all characters
       def reset_up
-        InitTrackerLogger.log.debug {"reset went"}
         characters.each do |character|
           character[:up] = false
         end
       end
 
+      # Sorts the characters
       def sort_characters
-        characters.sort_by! {|character| character[:number]}.reverse!
+        characters.sort_by! {|character| character[:order]}.reverse!
       end
 
+      # Sets the characters init order based on the dice. Re-rolls the dice and sets the order
       def set_character_init_order(character)
         number = nil
         count = 1
@@ -154,18 +169,19 @@ module InitTracker
           count = count + 1
         end
         character[:number] = number
+        character[:order] = number * ORDER_MULTIPLIER
       end
 
+      # Rolls the dice
       def roll(amount = 0, sides = 0)
         amount.to_i.times.sum { |t| SecureRandom.random_number(1..sides.to_i) }
       end
 
+      # When the init loads, look for any missing character attributes and add them
       def add_missing_attributes
-        InitTrackerLogger.log.debug {"adding missing attributes"}
         return if characters.empty?
-        return if characters.first.has_key?(:went)
-        InitTrackerLogger.log.debug {"calling reset_went"}
-        reset_went
+        return if characters.first.has_key?(:order)
+        set_order
       end
 
     end
